4c89d66c241d4511958b602adf5740955341d4d9
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-03-22 15:00:29 -0300

    produto final

--- Atividades/produtos.c
+++ Atividades/produtos.c
@@ -4,18 +4,21 @@
 
 #define MAX_PRODUTOS 100
 
-struct Produto {
+struct Produto
+{
     int codigo;
     char nome[50];
     float preco;
 };
 
-int main() {
+int main()
+{
     struct Produto produtos[MAX_PRODUTOS];
     int qtd_produtos = 0;
 
     // Cadastro de produtos
-    while (1) {
+    while (1)
+    {
         int codigo;
         char nome[50];
         float preco;
@@ -23,20 +26,24 @@ int main() {
         printf("Digite o codigo do produto (ou 0 para sair): ");
         scanf("%d", &codigo);
 
-        if (codigo == 0) {
+        if (codigo == 0)
+        {
             break;
         }
 
         // Verifica se o código já foi cadastrado
         int codigo_existente = 0;
-        for (int i = 0; i < qtd_produtos; i++) {
-            if (produtos[i].codigo == codigo) {
+        for (int i = 0; i < qtd_produtos; i++)
+        {
+            if (produtos[i].codigo == codigo)
+            {
                 codigo_existente = 1;
                 break;
             }
         }
 
-        if (codigo_existente) {
+        if (codigo_existente)
+        {
             printf("Erro: o codigo %d ja foi cadastrado!\n", codigo);
             continue;
         }
@@ -57,9 +64,31 @@ int main() {
     }
 
     printf("\nLista de produtos cadastrados:\n");
-    for (int i = 0; i < qtd_produtos; i++) {
-        printf("Codigo: %d
+    for (int i = 0; i < qtd_produtos; i++)
+    {
+        printf("Codigo: %d\tNome: %s\n", produtos[i].codigo, produtos[i].nome);
+    }
 
+    // Consulta de preço
+    int codigo_consulta;
+    printf("\nDigite o codigo do produto que deseja consultar o preco: ");
+    scanf("%d", &codigo_consulta);
 
+    int produto_encontrado = 0;
+    for (int i = 0; i < qtd_produtos; i++)
+    {
+        if (produtos[i].codigo == codigo_consulta)
+        {
+            printf("Preco do produto %s: %.2f\n", produtos[i].nome, produtos[i].preco);
+            produto_encontrado = 1;
+            break;
+        }
+    }
 
+    if (!produto_encontrado)
+    {
+        printf("Produto nao encontrado!\n");
+    }
+
+    return 0;
 }
