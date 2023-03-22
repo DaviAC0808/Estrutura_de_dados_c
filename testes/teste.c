9aadc08ec0e7bd0041a16394578dfe1b223bd83f
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-03-22 08:16:46 -0300

    teste

---
+++ testes/teste.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int verdadeiro, falso, var;
+   
+    verdadeiro = (15 > 20);
+    falso = (15 == 20);
+
+    printf("Digite um numero: %d", &var);
+    scanf(var);
+
+    if (var>4 && !(var<9)||var<=4)
+    {
+        sizeof(var);
+    }
+    
+}
+    
