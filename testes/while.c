dfe22f342687b4978586831590ea314e24879bae
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-03-22 10:11:50 -0300

    if.while

--- testes/while.c
+++ testes/while.c
@@ -5,15 +5,30 @@ int main()
 {
     int var;
 
-    printf("digite um valor entre 0 e 10: \n");
-    scanf("%d",&var);
+    printf("Digite um valor entre 0 e 10: ");
+    scanf("%d", &var);
 
+    //inicio de 'while'
     while (var < 0 || var > 10)
     {
-        printf("Valor invalido, digite o valor novamente.\n");
+        printf("Valor invalido, digite o valor novamente.\n\n");
         scanf("%d", &var);
-
     }
 
-    printf("O valor %d foi inserido com sucesso.", var);
+    printf("\nO valor %d foi inserido com sucesso.\n", var);
+
+    printf("\n\n--------------------\n\n");
+
+    // inicio de 'do while'
+    do
+    {
+        if (var < 0 || var > 10)
+        {
+            printf("Valor invalido, digite o valor novamente.\n\n");
+        }        
+        printf("Digite um valor entre 0 e 10: ");
+        scanf("%d", &var);
+    } while (var < 0 || var > 10);
+
+    printf("\nO valor %d foi inserido com sucesso.\n", var);
 }
