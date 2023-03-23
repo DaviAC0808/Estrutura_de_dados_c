fd125930855d64d843a466bc92dc36affab4159a
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-03-23 11:17:03 -0300

    v2

--- testes/structs.c
+++ testes/structs.c
@@ -16,33 +16,36 @@ int main()
     printf("1_ Cadarstrar produto.\n");
     printf("2_ Ler produto.\n");
     printf("0_ Sair.\n\n");
-    scanf("%d", n);
+    scanf("%d ", &n);
 
-    while (n < 0 || n > 2)
+    while (n != 0)
     {
-        printf("Valor invalido, digite o valor novamente.\n\n");
-        scanf("%d", &n);
-    }
+        while (n < 0 || n > 2)
+        {
+            printf("Valor invalido, digite o valor novamente.\n\n");
+            scanf("%d", &n);
+        }
 
-    if (n == 1)
-    {
-        printf("\nDigite o codigo do produto: ");
-        scanf("%d", &p.cod);
+        if (n == 1)
+        {
+            printf("\nDigite o codigo do produto: ");
+            scanf("%d", &p.cod);
 
-        printf("\nDigite o valor do produto: ");
-        scanf(" %f", &p.valor);
-    }
+            printf("\nDigite o valor do produto: ");
+            scanf(" %f", &p.valor);
+        }
 
-    if (n == 2 && p.cod == 0)
-    {
-        printf("O programa parou inesperadamente.");
+        if (n == 2 && p.cod == 0)
+        {
+            printf("O programa parou inesperadamente.");
 
-        erro = 1;
-    }
+            erro = 1;
+        }
 
-    if (n == 2 && erro != 1)
-    {
-        printf("%d", p.cod);
-        printf("R$ %.2f", p.valor);
+        if (n == 2 && erro != 1)
+        {
+            printf("%d", p.cod);
+            printf("R$ %.2f", p.valor);
+        }
     }
 }
