86113cb64e5cdcc2925dfa7ac3a15ec36091028c
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-03-24 15:25:23 -0300

    v2

---
+++ ponteiro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
+    int valor;
+    int *ptr1;
+    int **ptr2;
+    ptr1 = &valor;
+    ptr2 = &ptr1;
+
+    printf("Endereço de ptr1 = %x\n", &ptr1);
+    printf("Endereço de ptr2 = %x\n", &ptr2);
+    printf("Valor do ptr1 = %i\n", *ptr1);
+    printf("Valor do ptr2 = %i\n", *ptr2);
+}
