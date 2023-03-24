a6aec46d4f3401ec86f9210d048d3d2aa08a4aa5
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-03-24 15:16:01 -0300

    pt-br

--- testes/scanf.c
+++ testes/scanf.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
     int var;
 
     printf("Digite um dado:");
-    scanf("%d", var);
+    scanf("%d", &var);
+
+    printf("O valor do dado é: %d", var);
 
 
 }
