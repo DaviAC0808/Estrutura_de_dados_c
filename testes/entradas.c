a83075900ab0cfa9dc6987c8665d4ae323cdc488
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-04-12 13:07:54 -0300

    entradas

---
+++ testes/entradas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nDigite um dado: \n");
+    scanf("%d", i);
+    fflush(stdin);
+
+    printf("\nDigite um dado: \n");
+    gets(n);
+    fflush(stdin);
+
+    printf("\nDigite um dado: \n");
+    puts(m);
+    fflush(stdin);
+    
+    printf("\nDigite um dado: \n");
+    fgets(n);
+    fflush(stdin);
+    
+
+}
