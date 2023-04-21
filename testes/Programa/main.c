fab22206b646199a31071683ec6fec1645839961
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-04-21 09:26:32 -0300

    Poligonos.h.c.main

--- testes/Programa/main.c
+++ testes/Programa/main.c
@@ -2,65 +2,81 @@
 #include <stdlib.h>
 #include <locale.h>
 #include "poligonos.h"
+#include "poligonos.c"
 
-int main(int argc, char const *argv[])
+int main()
 {
     setlocale(LC_ALL, "Portuguese_Brazil");
     int n, a, b, c, r;
 
     printf("\n======POLIGONOS======\n");
     system("pause");
-
-    printf("1_Calcular o perímetro do Triângulo.\n");
-    printf("2_Calcular o perímetro do Retângulo.\n");
-    printf("3_Calcular o perímetro do Circulo.\n");
-    printf("4_Calcular o área do Triângulo.\n");
-    printf("5_Calcular o área do Triângulo.\n");
-    printf("6_Calcular o área do Triângulo.\n");
-    printf("7_Sair.\n");
-    scanf("%d", &n);
-
-    switch (n)
+    do
     {
-    case 1:
-        printf("Digite os lados do triângulo: ");
-        scanf("%d,%d,%d", &a, &b, &c);
-        fflush(stdin);
-        float CalPerimetroTri(float a, float b, float c);
-        break;
-    case 2:
-        printf("Digite os lados do triângulo: ");
-        scanf("%d,%d", &a, &b);
-        fflush(stdin);
-        float CalPerimetroRet(float a, float b);
-        break;
-    case 3:
-        printf("Digite os lados do triângulo: ");
-        scanf("%d", &r);
-        fflush(stdin);
-        float CalPerimetroCir(float r);
-        break;
-    case 4:
-        printf("Digite os lados do triângulo: ");
-        scanf("%d,%d", &b, &a);
-        fflush(stdin);
-        float CalAreaTri(float b, float a);
-        break;
-    case 5:
-        printf("Digite os lados do triângulo: ");
-        scanf("%d,%d", &b, &a);
-        fflush(stdin);
-        float CalAreaRet(float b, float a);
-        break;
-    case 6:
-        printf("Digite os lados do triângulo: ");
-        scanf("%d", &r);
-        fflush(stdin);
-        float CalAreaCir(float r);
-        break;
-    default:
-        printf("Valor incorreto, tente novamente.");
-    }
+        system("cls");
+        printf("1 - Calcular o perímetro do Triângulo.\n");
+        printf("2 - Calcular o perímetro do Retângulo.\n");
+        printf("3 - Calcular o perímetro do Circulo.\n");
+        printf("4 - Calcular o área do Triângulo.\n");
+        printf("5 - Calcular o área do Triângulo.\n");
+        printf("6 - Calcular o área do Triângulo.\n");
+        printf("7 - Sair.\n");
+        scanf("%d", &n);
+
+        switch (n)
+        {
+        case 1:
+            printf("Digite os lados do triângulo: ");
+            scanf("%d,%d,%d", &a, &b, &c);
+            fflush(stdin);
+            printf("Perímetro do Triângulo: %.2f\n", CalPerimetroTri(a, b, c));
+            system("pause");
+            break;
+
+        case 2:
+            printf("Digite os lados do triângulo: ");
+            scanf("%d,%d", &a, &b);
+            fflush(stdin);
+            printf("O perímetro do retângulo é: %.2f", CalPerimetroRet(a, b));
+            system("pause");
+            break;
+
+        case 3:
+            printf("Digite os lados do triângulo: ");
+            scanf("%d", &r);
+            fflush(stdin);
+            printf("O perímetro do círculo é: %.2f", CalPerimetroCir(r));
+            system("pause");
+            break;
+
+        case 4:
+            printf("Digite os lados do triângulo: ");
+            scanf("%d,%d", &b, &a);
+            fflush(stdin);
+            printf("A área do triângulo é: %.2f", CalAreaTri(b, a));
+            system("pause");
+            break;
+
+        case 5:
+            printf("Digite os lados do triângulo: ");
+            scanf("%d,%d", &b, &a);
+            fflush(stdin);
+            printf("A área do retângulo é: %.2f", CalAreaRet(b, a));
+            system("pause");
+            break;
+
+        case 6:
+            printf("Digite os lados do triângulo: ");
+            scanf("%d", &r);
+            fflush(stdin);
+            printf("A área do circulo é: %.2f", CalAreaCir(r));
+            system("pause");
+            break;
+
+        default:
+            printf("Valor incorreto, tente novamente.");
+        }
+    } while (n != 7);
 
     return 0;
 }
