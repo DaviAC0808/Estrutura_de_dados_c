aa08d659eb4487f53262924328a877b4c9b49f8b
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-04-20 22:23:03 -0300

    void vet

--- testes/functin/function_vet.c
+++ testes/functin/function_vet.c
@@ -2,13 +2,47 @@
 #include <stdlib.h>
 #include <locale.h>
 
+void imprime1(int v[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", v[i]);
+    }
+}
 
+void imprime2(int *v, int n);//protótipo de função
+
+
+void imprime3(int v[5])
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", v[i]);
+    }
+}
 
 int main()
 {
-    setlocale(LC_ALL,"Portuguese_Brazil");
-    int;
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int vet[5] = {1, 2, 3, 4, 5};
 
-    printf("\n\n");
-    scanf("%", );
+    puts("Primeiro imprime: ");
+    imprime1(vet, 5);
+
+    puts("\nSegundo imprime: ");
+    imprime2(vet, 5);
+
+    puts("\nTerceiro imprime: ");
+    imprime3(vet);
+}
+
+void imprime2(int *v, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", v[i]);
+    }
 }
