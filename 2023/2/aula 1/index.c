2ee5123ca9353bc638d52388d899f9a922eb6f6b
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-08-10 10:48:47 -0300

    add for

--- 2023/2/aula 1/index.c
+++ 2023/2/aula 1/index.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#define max 3
 
 typedef struct pessoa
 {
@@ -14,25 +15,29 @@ int main()
     setlocale(LC_ALL, "Portuguese_Brazil");
     pessoa p[3];
 
-    
+    for (int i = 0; i < max; i++)
+    {
 
-    printf("Informe o nome: ");
-    fflush(stdin);
-    gets(p.nome);
+        printf("Informe o nome: ");
+        fflush(stdin);
+        gets(p[i].nome);
 
-    printf("informe a idade: ");
-    scanf("%d", &p.idade);
-    
-    printf("informe o endereço: ");
-    fflush(stdin);
-    gets(p.end);
+        printf("informe a idade: ");
+        scanf("%d", &p[i].idade);
 
-//==========================================
+        printf("informe o endereço: ");
+        fflush(stdin);
+        gets(p[i].end);
+        printf("\n");
+    }
+    //==========================================
 
-printf("\n==========Dados==========\n");
+    printf("\n==========Dados==========\n");
 
-printf("Nome: %s\n", p.nome);
-printf("Idade: %d\n", p.idade);
-printf("Endereço: %s\n", p.end);
-
+    for (int i = 0; i < max; i++)
+    {
+        printf("\nNome: %s\n", p[i].nome);
+        printf("Idade: %d\n", p[i].idade);
+        printf("Endereço: %s\n", p[i].end);
+    }
 }
