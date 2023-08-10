3979513ebf488285ea056a87fc92859ad8677475
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-08-10 10:36:58 -0300

    struct  funcionando

--- 2023/2/aula 1/index.c
+++ 2023/2/aula 1/index.c
@@ -2,13 +2,37 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
+typedef struct pessoa
+{
+    char nome[50];
+    char end[50];
+    int idade;
+} pessoa;
 
 int main()
 {
-    setlocale(LC_ALL,"Portuguese_Brazil");
-    int;
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    pessoa p[3];
 
-    printf("\n\n");
-    scanf("%", );
+    
+
+    printf("Informe o nome: ");
+    fflush(stdin);
+    gets(p.nome);
+
+    printf("informe a idade: ");
+    scanf("%d", &p.idade);
+    
+    printf("informe o endereço: ");
+    fflush(stdin);
+    gets(p.end);
+
+//==========================================
+
+printf("\n==========Dados==========\n");
+
+printf("Nome: %s\n", p.nome);
+printf("Idade: %d\n", p.idade);
+printf("Endereço: %s\n", p.end);
+
 }
