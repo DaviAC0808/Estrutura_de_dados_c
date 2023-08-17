83652cd72a4afb6f1cd503aa4ad48aae1e185cb2
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-08-17 11:27:09 -0300

    teste de struct em struct

---
+++ 2023/2/aula 1/17_08.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct reg_end
+{
+    int num;
+    char rua;
+    char bairro;
+} reg_end;
+
+struct aluno
+{
+    int cpf;
+    char nome;
+    char sexo;
+    struct reg_end ende;
+
+} aluno;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int n;
+
+    printf("\nQuantos alunos existem? \n");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Endereço:");
+        fflush(stdin);
+        gets(aluno.ende.num);
+    }
+    printf("\nNumero: %d\n", aluno.ende.num);
+
+}
