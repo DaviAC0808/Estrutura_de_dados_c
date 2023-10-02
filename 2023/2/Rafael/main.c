45bc9e88ba19e1175e3d33d91a3f263c66d06a64
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-10-02 12:04:25 -0300

    1a aula

--- 2023/2/Rafael/main.c
+++ 2023/2/Rafael/main.c
@@ -2,11 +2,28 @@
 #include <stdlib.h>
 #include <locale.h>
 
+struct ponto
+{
+    int x;
+    int y;
+};
+
+struct reta
+{
+    struct ponto inicio;
+    struct ponto fim;
+};
+
 int main()
 {
-    setlocale(LC_ALL,"Portuguese_Brazil");
-    int;
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    struct carro c[10];
+
+    struct carro novoCarro;
+    novoCarro = CriaNovoCarro();
+
+    mostrar_carros(c, 10);
 
     printf("\n\n");
-    //scanf("%", );
+    // scanf("%", );
 }
