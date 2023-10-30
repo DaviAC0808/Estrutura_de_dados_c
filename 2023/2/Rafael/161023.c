d2095a2325fb1a1a0e896714d0c1902fe08921c3
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-10-30 10:46:27 -0300

    new.c

---
+++ 2023/2/Rafael/161023.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+    
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int;
+
+    printf("\n\n");
+    //scanf("%", );
+}
